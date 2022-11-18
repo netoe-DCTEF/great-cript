b9eab0c20fa9ccb453b6b73bcdba3903ba6f8aad
weealDCTEF <elynetobr@outlook.com>
2022-11-17 21:51:50 -0300

    master

---
+++ random.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+
+
+void genSeed()
+{
+    srand(time(NULL));
+}
+
+int int_genRnd(const int limit)
+{
+    return rand() % limit;
+}
+
+int int_genRandNxtTo(const int start,const int limit)
+{
+    int number;
+
+    do
+    {
+        number = rand() % limit;
+    }
+    while(number < start);
+
+    return number;
+}
+
+void int_genRand_negative(const int limit)
+{
+    return int_genRnd(limit) * -1;
+}
+
